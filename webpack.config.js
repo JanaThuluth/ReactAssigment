7b62bea2814762e25e65133a882d1a0b3c740d76
JanaThuluth <s12325336@stu.najah.edu>
2025-03-20 13:40:49 +0200

    update-f

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -42,7 +43,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
-    
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "public/assets", to: "assets" }, 
+      ],
+    }),
   
   ],
   devServer: {
